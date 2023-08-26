9160ea1017714d4ff3e178c77d1d9f91cab76486
Stanislav Prokofjev <Prokostas@gmail.com>
2023-08-26 23:10:46 +0300

    add add minicss plugin ,set up css module

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,30 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      //Creates 'style' nodes from JS strings
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      //Translate CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev 
+            ? '[path][name]__[local]--[hash:base64:5]' 
+            : '[hash:base64:8]'
+          },
+        }
+      },
+      //Compile Sass to CSS
+      "sass-loader",
+    ],
+  }
 
   const typescriptLoader =  {
     test: /\.tsx?$/,
@@ -10,5 +34,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
   return [
     typescriptLoader,
+    cssLoader,
   ]
 }
