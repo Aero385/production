e61305843b0c33be47ed46e8d91b833014d90390
Stanislav Prokofjev <Prokostas@gmail.com>
2023-09-01 00:40:59 +0300

    created sidebar, added i18 languageSwitcher

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,29 @@
+import {useTranslation} from "react-i18next";
+import React from "react";
+import Button, {ThemeButton} from "shared/ui/Button/Button";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './LangSwitcher.module.scss';
+
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+const LangSwitcher = ({className}: LangSwitcherProps) => {
+        const {t, i18n} = useTranslation();
+
+        const changeLang = () => {
+            i18n.changeLanguage(i18n.language === 'eng' ? 'ru' : 'eng')
+        }
+        return (
+                <Button
+                    className={classNames(cls.LangSwitcher, {}, [className])}
+                    theme={ThemeButton.CLEAR}
+                    onClick={changeLang}
+                >
+                    {t('language')}
+                </Button>
+        )
+    }
+
+export default LangSwitcher;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 
 interface NavbarProps {
@@ -11,7 +10,7 @@ interface NavbarProps {
 const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <ThemeSwitcher/>
+
             <div className={cls.link}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
                     Main
